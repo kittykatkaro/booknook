9296f9e5627a9cfb616bf243ffc5c8a746b8809b
Karo <karoogorek@gmail.com>
2025-06-16 22:28:43 +0200

    Set up Express server with MongoDB connection and book routes

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const bookRoutes = require('./routes/bookRoutes');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/books', bookRoutes);
 
 app.get('/', (req, res) => {
 	res.send('API is running');
@@ -12,3 +15,11 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const mongoose = require('mongoose');
+mongoose
+	.connect(process.env.MONGO_URI, {
+		serverSelectionTimeoutMS: 10000,
+	})
+	.then(() => console.log('MongoDB connected 🚀'))
+	.catch((err) => console.error('MongoDB connection error:', err));
